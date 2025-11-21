79f399373b9941029340d11deb31e53761a53411
seolmiseon <budaxige@gmail.com>
2025-11-21 15:13:52 +0900

    fix: Configure Next.js static export and update Firebase hosting to use out directory

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,6 +1,6 @@
-/** @type {import('next').NextConfig} */
+
 const nextConfig = {
-   
+    output: 'export',   
     webpack: (config) => {
         config.resolve.fallback = {
             ...config.resolve.fallback,
